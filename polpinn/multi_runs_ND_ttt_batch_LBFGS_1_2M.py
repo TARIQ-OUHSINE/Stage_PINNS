684205085957067d8a70f7948d51316123156cea
Tarik <tarikouhsine@gmail.com>
2025-07-22 01:14:27 +0200

    Modifications des scripts LBFGS pour cluster

--- polpinn/multi_runs_ND_ttt_batch_LBFGS_1_2M.py
+++ polpinn/multi_runs_ND_ttt_batch_LBFGS_1_2M.py
@@ -178,7 +178,7 @@ def cost_enhanced_full_batch(model, F_solid, F_liquid, S_f, S_j, X_fick_total, X
 
     # Pondération MANUELLE agressive
     w_data = 1.0
-    w_phys = 10.0
+    w_phys = 100.0
     
     total_loss = (w_data * L_yz) + (w_data * L_solide) + (w_phys * L_ini) + (w_phys * L_gradient_nul) + (w_phys * L_fick_s) + (w_phys * L_fick_l)
 
