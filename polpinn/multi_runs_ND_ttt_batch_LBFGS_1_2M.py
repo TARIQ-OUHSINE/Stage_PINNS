a6dbc4b8a4e7a0641d370863a58099d9dcfe00f1
Tarik <tarikouhsine@gmail.com>
2025-07-23 18:35:42 +0200

    Modifications des scripts LBFGS pour cluster

--- polpinn/multi_runs_ND_ttt_batch_LBFGS_1_2M.py
+++ polpinn/multi_runs_ND_ttt_batch_LBFGS_1_2M.py
@@ -452,7 +452,7 @@ if __name__ == "__main__":
         "D_f": D_f_calculated, 
         "D_j": D_j_calculated,
         "T_1_f": exp_data.get("T_1_f", 300.0), 
-        "T_1_j": exp_data[solid_data_key]["TB_j"], #exp_data.get("T_1_j", 3.0),
+        "T_1_j": exp_data[solid_data_key]["TB_j"],  #exp_data.get("T_1_j", 3.0),
         "P0_f": 1.0, 
         "P0_j": exp_data[solvent_data_key]["P0_j"],
         "def_t": max(exp_data[solid_data_key]["t"]),
